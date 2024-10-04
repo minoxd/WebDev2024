a639eb0b2d044bb177181acca1d7d30adf3e4258
justxoai <tuti12032004@gmail.com>
2024-10-04 23:46:49 +0700

    JS

--- photo.js
+++ photo.js
@@ -1,6 +1,7 @@
 // Select relevant HTML elements
-const filterButtons = document.querySelectorAll(".filter-buttons button"); 
-const filterableCards = document.querySelectorAll(".filterable-cards .card"); 
+const filterButtons = document.querySelectorAll(".filter-buttons button");
+const filterableCards = document.querySelectorAll(".filterable-cards .card");
+const cardColumn = document.querySelector('.column');
 
 // Function to filter cards based on filter buttons
 const filterCards = (e) => {
@@ -9,11 +10,19 @@ const filterCards = (e) => {
 
     filterableCards.forEach(card => {
         // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
-        if(card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+        if (card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+            if (e.target.dataset.filter === "all") {
+                cardColumn.style.flexDirection = "column"; // Change back to column for "all"
+            } else {
+                cardColumn.style.flexDirection = "row"; // Change to row for other filters
+            }
             return card.classList.replace("hide", "show");
+
         }
         card.classList.add("hide");
     });
+
+
 }
 
 filterButtons.forEach(button => button.addEventListener("click", filterCards));
@@ -21,7 +30,7 @@ filterButtons.forEach(button => button.addEventListener("click", filterCards));
 
 
 
-document.getElementById('showMoreBtn').addEventListener('click', function() {
+document.getElementById('showMoreBtn').addEventListener('click', function () {
     const hiddenButtons = document.querySelector('.hidden-buttons');
     const moreButton = document.getElementById('showMoreBtn');
 
@@ -33,4 +42,12 @@ document.getElementById('showMoreBtn').addEventListener('click', function() {
         hiddenButtons.style.display = "none";
         moreButton.textContent = "More";
     }
+});
+
+
+
+const logo = document.getElementById('pageLogo');
+
+logo.addEventListener('click', () => {
+    window.location.reload(); // This reloads the page
 });
